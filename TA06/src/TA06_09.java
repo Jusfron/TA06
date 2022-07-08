571d1c20709de540993582c9dd912c17743b6a3b
David Marcos <david.13254@gmail.com>
2022-07-08 12:03:50 +0200

    ejercicio 9

--- TA06/src/TA06_09.java
+++ TA06/src/TA06_09.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class TA06_09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = new int[10];
+		rellenarValores(nums);
+		mostrarValores(nums);
 	}
 
+	private static void rellenarValores(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = (int)(Math.random()*10);
+		}
+	}
+	
+	private static void mostrarValores(int[] nums) {
+		int suma = 0;
+		for(int i = 0; i < nums.length; i++) {
+			System.out.println("Indice: "+i+" Numero: "+nums[i]);
+			suma += nums[1];
+		}
+		System.out.println("Suma total: "+suma);
+	}
+	
 }
