24f761f07e6a19ed9ab59fec386e360ae9c00a32
David Marcos <david.13254@gmail.com>
2022-07-08 09:49:51 +0200

    Ejercicio 4

---
+++ TA06/src/TA06_4.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class TA06_4 {
+
+	public static void main(String[] args) {
+		int num;
+		num = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero"));
+		JOptionPane.showMessageDialog(null, "El factorial de "+num+" es "+calcularFactorial(num));
+	}
+	
+	private static int calcularFactorial(int num) {
+		int factorial=1;
+		for(int i = 1; i < num+1; i++) {
+			factorial = factorial * i;
+		}
+		return factorial;
+	}
+
+}
